21b4f9533166a613982513d544dd6d08967dab2f
pierce <pierceshih@gmail.com>
2019-03-06 12:40:44 +0800

    add search bar UI and event function

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-(function () {
-    // write your code here
-    // Declare API 變數
-    const BASE_URL = 'https://movie-list.alphacamp.io/'
-    const INDEX_URL = BASE_URL + 'api/v1/movies/'
-    const POSTERS_URL = BASE_URL + '/posters/'
-    const data = []
+// Declare API 變數
+const BASE_URL = 'https://movie-list.alphacamp.io/';
+const INDEX_URL = BASE_URL + 'api/v1/movies/';
+const POSTERS_URL = BASE_URL + '/posters/';
+const data = [];
 
+(function () {
     // Catch API Data
     axios.get(INDEX_URL).then((response) => {
         data.push(...response.data.results)
@@ -14,49 +13,51 @@
         displayDataList(data);
     }).catch((error) => console.log(error))
 
-    // Display Movie List
-    const dataPanel = document.getElementById('data-panel')
+})()
 
-    // 監聽事件，促發 POP-UP 內容
-    dataPanel.addEventListener('click', (e) => {
-        // 當使用者點擊 card-footer 中的 button
-        if (e.target.matches('.btn-show-movie')) {
-            // dataset 抓出特定資料，以利後續操作
-            console.log(e.target.dataset.id);
-            let id = e.target.dataset.id;
-            showMovie(id);
-        }
-    })
+// Display Movie List
+const dataPanel = document.getElementById('data-panel')
 
-    function showMovie(id) {
-        // get element
-        const modalTitle = document.getElementById('show-movie-title')
-        const modalImage = document.getElementById('show-movie-image')
-        const modalDate = document.getElementById('show-movie-date')
-        const modalDescription = document.getElementById('show-movie-description')
-
-        // set request url
-        const url = INDEX_URL + id
-        console.log(url);
-
-        // send request to show api
-        axios.get(url).then((response) => {
-            const data = response.data.results;
-            console.log(data)
-
-            // insert data to modal ui
-            modalTitle.textContent = data.title;
-            modalImage.innerHTML = `<img src="${POSTERS_URL}${data.image}" class="img-fluid" alt="Responsive image">`
-            modalDate.textContent = `release at: ${data.release_date}`
-            modalDescription.textContent = `${data.description}`
-        })
+// 監聽事件，促發 POP-UP 內容
+dataPanel.addEventListener('click', (e) => {
+    // 當使用者點擊 card-footer 中的 button
+    if (e.target.matches('.btn-show-movie')) {
+        // dataset 抓出特定資料，以利後續操作
+        console.log(e.target.dataset.id);
+        let id = e.target.dataset.id;
+        showMovie(id);
     }
+})
+
+function showMovie(id) {
+    // get element
+    const modalTitle = document.getElementById('show-movie-title')
+    const modalImage = document.getElementById('show-movie-image')
+    const modalDate = document.getElementById('show-movie-date')
+    const modalDescription = document.getElementById('show-movie-description')
+
+    // set request url
+    const url = INDEX_URL + id
+    console.log(url);
+
+    // send request to show api
+    axios.get(url).then((response) => {
+        const data = response.data.results;
+        console.log(data)
+
+        // insert data to modal ui
+        modalTitle.textContent = data.title;
+        modalImage.innerHTML = `<img src="${POSTERS_URL}${data.image}" class="img-fluid" alt="Responsive image">`
+        modalDate.textContent = `release at: ${data.release_date}`
+        modalDescription.textContent = `${data.description}`
+    })
+}
 
 
-    function displayDataList(data) {
-        let htmlContent = ''
-        data.forEach((item, index) => {
-            htmlContent += `
+function displayDataList(data) {
+    let htmlContent = ''
+    data.forEach((item, index) => {
+        htmlContent += `
             <div class="col-sm-3">
                 <div class="card mb-2">
                     <img class="card-img-top" src="${POSTERS_URL}${item.image}" alt="Card image cap">
@@ -69,7 +70,23 @@
                 </div>
             </div>
         `
-        });
-        dataPanel.innerHTML = htmlContent;
-    }
-})()
+    });
+    dataPanel.innerHTML = htmlContent;
+}
+
+// Search Bar
+const searchBtn = document.getElementById('submit-search');
+const searchInput = document.getElementById('search')
+
+// SearchBtn 事件綁定
+searchBtn.addEventListener('click', event => {
+    // 移除預設事件
+    event.preventDefault();
+    let results = [];
+
+    const regex = new RegExp(searchInput.value, 'i')
+    results = data.filter(movie => movie.title.match(regex))
+    // console.log(results);
+
+    displayDataList(results);
+})
